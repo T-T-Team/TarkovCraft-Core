265e62a1d089bbf88f35856703c49c357bff9ae1
Toma <novotny.tom96@gmail.com>
2025-08-23 19:57:14 +0200

    Remove testing weight assignments

--- src/main/java/tnt/tarkovcraft/core/TarkovCraftCore.java
+++ src/main/java/tnt/tarkovcraft/core/TarkovCraftCore.java
@@ -2,14 +2,11 @@ package tnt.tarkovcraft.core;
 
 import dev.toma.configuration.Configuration;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.ItemLike;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.ModifyDefaultComponentsEvent;
 import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 import net.neoforged.neoforge.registries.NewRegistryEvent;
 import org.apache.logging.log4j.LogManager;
@@ -24,8 +21,6 @@ import tnt.tarkovcraft.core.common.skill.SkillSystem;
 import tnt.tarkovcraft.core.common.statistic.DisplayStatistic;
 import tnt.tarkovcraft.core.common.weight.WeightSystem;
 import tnt.tarkovcraft.core.network.TarkovCraftCoreNetwork;
-
-import java.util.function.BiConsumer;
 
 @Mod(TarkovCraftCore.MOD_ID)
 public final class TarkovCraftCore {
@@ -47,7 +42,6 @@ public final class TarkovCraftCore {
         modEventBus.addListener(this::registerCustomDatapackRegistries);
         modEventBus.addListener(TarkovCraftCoreNetwork::onRegistration);
         modEventBus.addListener(WeightSystem.INSTANCE::registerDefaultProviders);
-        modEventBus.addListener(this::assignWeight);
 
         // Neoforge event listeners
         NeoForge.EVENT_BUS.register(new TarkovCraftCoreEventHandler());
@@ -103,15 +97,5 @@ public final class TarkovCraftCore {
 
     private void setup(FMLCommonSetupEvent event) {
         WeightSystem.INSTANCE.init();
-    }
-
-    private void assignWeight(ModifyDefaultComponentsEvent event) {
-        BiConsumer<ItemLike, Integer> weightSetter = (it, w) -> event.modify(it, builder -> builder.set(CoreItemDataComponents.WEIGHT.get(), w).build());
-
-        weightSetter.accept(Items.BUNDLE, 500);
-        weightSetter.accept(Items.COBBLESTONE, 750);
-        weightSetter.accept(Items.DIAMOND_HELMET, 2500);
-        weightSetter.accept(Items.NETHERITE_SWORD, 3000);
-        weightSetter.accept(Items.ARROW, 500);
     }
 }
