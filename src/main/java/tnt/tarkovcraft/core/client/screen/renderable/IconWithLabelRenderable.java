38ab13a8bc88b08c38cd8b62e22e5b3737ea602e
Toma <novotny.tom96@gmail.com>
2025-09-14 12:57:22 +0200

    More icon with label properties

--- src/main/java/tnt/tarkovcraft/core/client/screen/renderable/IconWithLabelRenderable.java
+++ src/main/java/tnt/tarkovcraft/core/client/screen/renderable/IconWithLabelRenderable.java
@@ -14,11 +14,28 @@ public class IconWithLabelRenderable extends AbstractRenderable {
     private final IconWithLabel iconWithLabel;
     private final HorizontalAlignment alignment;
 
+    private int iconSize;
+    private int iconOffset = 2;
+    private boolean textShadow = true;
+
     public IconWithLabelRenderable(Font font, int x, int y, int width, int height, HorizontalAlignment alignment, IconWithLabel iconWithLabel) {
         super(x, y, width, height);
         this.font = font;
         this.alignment = alignment;
         this.iconWithLabel = iconWithLabel;
+        this.iconSize = this.iconWithLabel.icon() != null ? this.height : 0;
+    }
+
+    public void setIconSize(int iconSize) {
+        this.iconSize = iconSize;
+    }
+
+    public void setIconOffset(int iconOffset) {
+        this.iconOffset = iconOffset;
+    }
+
+    public void setTextShadow(boolean textShadow) {
+        this.textShadow = textShadow;
     }
 
     @Override
@@ -27,14 +44,14 @@ public class IconWithLabelRenderable extends AbstractRenderable {
         Component label = this.iconWithLabel.getLabel();
         int itemWidth = this.font.width(label);
         if (icon != null) {
-            itemWidth += 2 + this.height;
+            itemWidth += this.iconOffset + this.iconSize;
         }
         int left = (int) this.alignment.getPosition(this.getX(), this.getRight(), itemWidth);
         if (icon != null) {
-            RenderUtils.blitFull(guiGraphics, icon, left, this.y, left + this.height, this.y + this.height, this.iconWithLabel.iconColor());
-            left += this.height + 2;
+            RenderUtils.blitFull(guiGraphics, icon, left, this.y, left + this.iconSize, this.y + this.iconSize, this.iconWithLabel.iconColor());
+            left += this.iconSize + this.iconOffset;
         }
 
-        guiGraphics.drawString(this.font, this.iconWithLabel.getLabel(), left, this.y - (this.height - this.font.lineHeight) / 2 + 1, this.iconWithLabel.labelColor(), true);
+        guiGraphics.drawString(this.font, this.iconWithLabel.getLabel(), left, this.y - (this.height - this.font.lineHeight) / 2 + 1, this.iconWithLabel.labelColor(), this.textShadow);
     }
 }
