042c8824daea88b372fe8282e97f68dde4b2e1fe
Toma <novotny.tom96@gmail.com>
2026-04-03 13:54:47 +0200

    More number provider codecs

--- src/main/java/tnt/tarkovcraft/core/common/data/number/NumberProviderType.java
+++ src/main/java/tnt/tarkovcraft/core/common/data/number/NumberProviderType.java
@@ -5,6 +5,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.Mth;
 import tnt.tarkovcraft.core.common.data.duration.Duration;
 import tnt.tarkovcraft.core.common.init.CoreRegistries;
 
@@ -16,6 +17,11 @@ public record NumberProviderType<N extends NumberProvider>(Identifier identifier
     public static final Codec<NumberProvider> ID_CODEC = CoreRegistries.NUMBER_PROVIDER.byNameCodec().dispatch(NumberProvider::getType, NumberProviderType::codec);
     public static final Codec<NumberProvider> VALUE_CODEC = valueCodec(Codec.DOUBLE);
     public static final Codec<NumberProvider> CODEC = durationCodec(ExtraCodecs.NON_NEGATIVE_FLOAT);
+    public static final Codec<Integer> INT_CODEC = numberCodec(Codec.INT, NumberProvider::intValue);
+    public static final Codec<Long> LONG_CODEC = numberCodec(Codec.LONG, NumberProvider::longValue);
+    public static final Codec<Float> FLOAT_CODEC = numberCodec(Codec.FLOAT, NumberProvider::floatValue);
+    public static final Codec<Double> DOUBLE_CODEC = numberCodec(Codec.DOUBLE, NumberProvider::getNumber);
+    public static final Codec<Float> PERCENT_CODEC = numberCodec(Codec.floatRange(0.0F, 1.0F), prov -> Mth.clamp(prov.floatValue(), 0.0F, 1.0F));
 
     public static <N extends Number> Codec<NumberProvider> valueCodec(Codec<N> codec) {
         return Codec.either(ID_CODEC, codec)
@@ -25,6 +31,11 @@ public record NumberProviderType<N extends NumberProvider>(Identifier identifier
     public static <N extends Number> Codec<NumberProvider> durationCodec(Codec<N> codec) {
         return Codec.either(valueCodec(codec), Duration.STRING_CODEC)
                 .xmap(v -> v.map(Function.identity(), DurationNumberProvider::new), Either::left);
+    }
+
+    public static <N extends Number> Codec<N> numberCodec(Codec<N> codec, Function<NumberProvider, N> toNumber) {
+        return Codec.either(codec, ID_CODEC)
+                .xmap(v -> v.map(Function.identity(), toNumber), Either::left);
     }
 
     @Override
