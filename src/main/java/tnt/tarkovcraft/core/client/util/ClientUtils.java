71d5cdb135ebc0e1882f5e16f507cce8d55b5501
Toma <novotny.tom96@gmail.com>
2026-05-01 20:06:59 +0200

    Maven improvements

--- src/main/java/tnt/tarkovcraft/core/client/util/ClientUtils.java
+++ src/main/java/tnt/tarkovcraft/core/client/util/ClientUtils.java
@@ -3,10 +3,13 @@ package tnt.tarkovcraft.core.client.util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.resources.ResourceLocation;
 
 import java.util.Objects;
 
 public final class ClientUtils {
+
+    public static final ResourceLocation SLOT_SPRITE = ResourceLocation.withDefaultNamespace("container/slot");
 
     public static RegistryAccess getClientRegistryAccess() {
         Minecraft mc = Minecraft.getInstance();
